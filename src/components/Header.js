da8ed091a76c3b3e00067859e98b1c32c19f326b
tri <tridesignnm@gmail.com>
2022-07-09 19:57:39 +0700

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,9 @@
-import { AppBar, Toolbar, Grid } from "@mui/material";
+import { AppBar, Toolbar, Grid, IconButton,InputBase,Badge,TextField  } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
+// import { Badge } from "@mui/icons-material";
 
 const Header = () => {
     return ( 
@@ -6,11 +11,28 @@ const Header = () => {
             
             <Toolbar>
                 <Grid container>
-                    <Grid item sm={4} style={{border: '1px solid #333'}}>
-                        1
+                    <Grid item style={{border: '1px solid #333'}}>
+                        <InputBase fullWidth></InputBase>
+                        {/* <TextField id="outlined-basic" label="nhap ten vao " variant="standard" />  */}
                     </Grid>
-                    <Grid item sm={8} style={{border: '1px solid #Fff'}}>
-                        2
+                    <Grid item sm style={{border: '1px solid #333'}}>
+
+                    </Grid>
+                    <Grid item style={{border: '1px solid #Fff'}}>
+                        <IconButton color="secondary"><AddShoppingCartIcon /></IconButton>
+                        <IconButton>
+                            <Badge badgeContent={5} color="secondary">
+                                <NotificationsNoneIcon color="action" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                            <Badge badgeContent={5}>
+                                <ChatBubbleOutlineIcon color="action" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton>
+                           <PowerSettingsNewIcon color="action" />
+                        </IconButton>
                     </Grid>
                 </Grid>
             </Toolbar>
